b7c7549a7ea11c27c3bb66250be178027cc9e920
Vincenzo Sicurella <sicurella12@gmail.com>
2021-12-18 15:26:07 -0500

    sketched out different assignment modes

--- Source/MultimapperCommon.h
+++ Source/MultimapperCommon.h
@@ -25,14 +25,40 @@ namespace Multimapper
 
 	enum class MappingMode
 	{
-		Manual = 0,
-		Auto
+		Manual = 0,	// Manually set tuning table mapping
+		Auto		// Create tuning table mapping based on tuning
 	};
 
 	enum class MappingType
 	{
-		Linear,
-		Periodic,
+		Linear,		// Map root extends linearly in either direction
+		Periodic,	// Map root designated start of period and extends linearly up to 128 notes in either direction, then restarts a period multiple away from the root
 		Custom
+	};
+
+	enum class ChannelMode
+	{
+		Mono = 1,		// Voice limit of 1
+		FirstAvailable,	// Finds the first available voice from Channel 1
+		RoundRobin,		// Finds the first available voice from last channel assigned
+	};
+
+	enum class MpeZone
+	{
+		Omnichannel,	// Legacy mode, use all 16 MIDI channels
+		Lower,			// Channel 1 is the global channel
+		Upper			// Channel 16 is the global channel
+	};
+
+	enum class MidiMode
+	{
+		Mono = 4, // Strictly one note per channel is assigned
+		Poly = 3, // Multiple notes on a channel, but only if the pitchbend is the same
+	};
+
+	enum class VoiceLimit
+	{
+		Ignore,		// Ignore new notes if voice limit is met
+		Overwrite,	// Overwrite oldest note if voice limit is met
 	};
 }
