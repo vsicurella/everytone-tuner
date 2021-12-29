db19d4873d96ea75ca53efe40908fe20d43f3004
Vincenzo Sicurella <sicurella12@gmail.com>
2021-12-29 00:09:24 -0500

    Moved period to info bar (likely temporary)

--- Source/ui/InfoBar.h
+++ Source/ui/InfoBar.h
@@ -20,6 +20,7 @@ class InfoBar  : public juce::Component
     std::unique_ptr<juce::TextButton> actionButton;
     std::unique_ptr<juce::Label> nameValueLabel;
     std::unique_ptr<juce::Label> sizeValueLabel;
+    std::unique_ptr<juce::Label> periodValueLabel;
 
     bool buttonIsBack = false;
 
@@ -47,10 +48,15 @@ public:
         sizeValueLabel = std::make_unique<juce::Label>("sizeLabel", "");
         sizeValueLabel->setJustificationType(juce::Justification::centred);
         addAndMakeVisible(*sizeValueLabel);
+
+        periodValueLabel = std::make_unique<juce::Label>("periodLabel", "");
+        periodValueLabel->setJustificationType(juce::Justification::centred);
+        addAndMakeVisible(*periodValueLabel);
     }
 
     ~InfoBar() override
     {
+        periodValueLabel = nullptr;
         sizeValueLabel = nullptr;
         nameValueLabel = nullptr;
         actionButton = nullptr;
@@ -67,10 +73,22 @@ public:
         auto h = getHeight();
 
         auto buttonWidth = h * 1.5;
-        
-        actionButton->setBounds(0, 0, buttonWidth, h);
-        sizeValueLabel->setBounds(juce::roundToInt(w - buttonWidth), 0, buttonWidth, h);
-        nameValueLabel->setBounds(getLocalBounds().withX(actionButton->getRight()).withRight(sizeValueLabel->getX()));
+
+        auto margin = h * 0.15f;
+
+        auto sizeWidth = sizeValueLabel->getFont().getStringWidth(sizeValueLabel->getText()) + margin;
+        auto periodWidth = periodValueLabel->getFont().getStringWidth(periodValueLabel->getText()) + margin;
+
+        auto nameWidth = w - (buttonWidth + sizeWidth + periodWidth) - margin;
+
+        juce::FlexBox flexbox;
+
+        flexbox.items.add(juce::FlexItem(buttonWidth, h, *actionButton).withAlignSelf(juce::FlexItem::AlignSelf::center));
+        flexbox.items.add(juce::FlexItem(nameWidth, h, *nameValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
+        flexbox.items.add(juce::FlexItem(sizeWidth, h, *sizeValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
+        flexbox.items.add(juce::FlexItem(periodWidth, h, *periodValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
+
+        flexbox.performLayout(getLocalBounds());
     }
 
     void setDisplayedTuning(const MappedTuning* tuning)
@@ -78,11 +96,20 @@ public:
         nameValueLabel->setText(tuning->getName(), juce::NotificationType::dontSendNotification);
 
         // Probably should be abstracted
-        auto size = tuning->getTuning()->getVirtualSize();
-        if (size == 0)
-            size = tuning->getTuningSize();
+        auto sizeString = juce::String(tuning->getTuningSize());
+        auto sizeDetail = tuning->getTuning()->getVirtualSize();
+        if (sizeDetail != 0.0)
+            sizeString += " (" + juce::String(sizeDetail) + ")";
+        
+        sizeString += " steps to ";
+        sizeValueLabel->setText(sizeString, juce::NotificationType::dontSendNotification);
 
-        sizeValueLabel->setText(juce::String((int)size), juce::NotificationType::dontSendNotification);
+        auto periodString = juce::String(tuning->getTuning()->getPeriodCents());
+        auto periodDetail = tuning->getTuning()->getVirtualPeriod();
+        if (periodDetail != 0.0)
+            periodString += " (" + juce::String(periodDetail) + ")";
+        periodValueLabel->setText(periodString, juce::NotificationType::dontSendNotification);
+
         resized();
     }
 
