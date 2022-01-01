39feef2622b7e7b4c34037e156eb6382c132ff99
Vincenzo Sicurella <sicurella12@gmail.com>
2022-01-01 15:05:42 -0500

    Simplified info bar with just a summary label

--- Source/ui/InfoBar.h
+++ Source/ui/InfoBar.h
@@ -19,10 +19,12 @@ class InfoBar  : public juce::Component
 {
     std::unique_ptr<juce::TextButton> actionButton;
     std::unique_ptr<juce::Label> nameValueLabel;
-    std::unique_ptr<juce::Label> sizeValueLabel;
-    std::unique_ptr<juce::Label> periodValueLabel;
+    //std::unique_ptr<juce::Label> sizeValueLabel;
+    //std::unique_ptr<juce::Label> periodValueLabel;
 
     bool buttonIsBack = false;
+
+    juce::String mappedTuningSummary;
 
 public:
     InfoBar(juce::ApplicationCommandManager* cmdManager)
@@ -45,19 +47,19 @@ public:
         nameValueLabel->setJustificationType(juce::Justification::centred);
         addAndMakeVisible(*nameValueLabel);
 
-        sizeValueLabel = std::make_unique<juce::Label>("sizeLabel", "");
-        sizeValueLabel->setJustificationType(juce::Justification::centred);
-        addAndMakeVisible(*sizeValueLabel);
+        //sizeValueLabel = std::make_unique<juce::Label>("sizeLabel", "");
+        //sizeValueLabel->setJustificationType(juce::Justification::centred);
+        //addAndMakeVisible(*sizeValueLabel);
 
-        periodValueLabel = std::make_unique<juce::Label>("periodLabel", "");
-        periodValueLabel->setJustificationType(juce::Justification::centred);
-        addAndMakeVisible(*periodValueLabel);
+        //periodValueLabel = std::make_unique<juce::Label>("periodLabel", "");
+        //periodValueLabel->setJustificationType(juce::Justification::centred);
+        //addAndMakeVisible(*periodValueLabel);
     }
 
     ~InfoBar() override
     {
-        periodValueLabel = nullptr;
-        sizeValueLabel = nullptr;
+        //periodValueLabel = nullptr;
+        //sizeValueLabel = nullptr;
         nameValueLabel = nullptr;
         actionButton = nullptr;
     }
@@ -76,42 +78,48 @@ public:
 
         auto margin = h * 0.15f;
 
-        auto sizeWidth = sizeValueLabel->getFont().getStringWidth(sizeValueLabel->getText()) + margin;
-        auto periodWidth = periodValueLabel->getFont().getStringWidth(periodValueLabel->getText()) + margin;
+        //auto sizeWidth = sizeValueLabel->getFont().getStringWidth(sizeValueLabel->getText()) + margin;
+        //auto periodWidth = periodValueLabel->getFont().getStringWidth(periodValueLabel->getText()) + margin;
 
-        auto nameWidth = w - (buttonWidth + sizeWidth + periodWidth) - margin;
+        //auto nameWidth = w - (buttonWidth + sizeWidth + periodWidth) - margin;
+
+        auto nameWidth = w - buttonWidth;
 
         juce::FlexBox flexbox;
 
         flexbox.items.add(juce::FlexItem(buttonWidth, h, *actionButton).withAlignSelf(juce::FlexItem::AlignSelf::center));
         flexbox.items.add(juce::FlexItem(nameWidth, h, *nameValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
-        flexbox.items.add(juce::FlexItem(sizeWidth, h, *sizeValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
-        flexbox.items.add(juce::FlexItem(periodWidth, h, *periodValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
+        //flexbox.items.add(juce::FlexItem(sizeWidth, h, *sizeValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
+        //flexbox.items.add(juce::FlexItem(periodWidth, h, *periodValueLabel).withAlignSelf(juce::FlexItem::AlignSelf::center));
 
         flexbox.performLayout(getLocalBounds());
     }
 
     void setDisplayedTuning(const MappedTuningTable* tuning)
     {
-        nameValueLabel->setText(tuning->getName(), juce::NotificationType::dontSendNotification);
 
-        juce::String periodString = tuning->getPeriodString();
-        juce::String sizeString = tuning->getSizeString();
+        juce::StringArray tokens;
 
-        // Probably should be abstracted
-        //auto sizeString = juce::String(tuning->getTuningSize());
-        //auto sizeDetail = tuning->getTuning()->getVirtualSize();
-        //if (sizeDetail != 0.0)
-        //    sizeString += " (" + juce::String(sizeDetail) + ")";
-        //
-        sizeString += " steps to ";
-        sizeValueLabel->setText(sizeString, juce::NotificationType::dontSendNotification);
+        if (tuning->getName().isNotEmpty())
+            tokens.add(tuning->getName());
+        else
+            tokens.add("untitled");
 
-        //auto periodString = juce::String(tuning->getTuning()->getPeriodCents());
-        //auto periodDetail = tuning->getTuning()->getVirtualPeriod();
-        //if (periodDetail != 0.0)
-        //    periodString += " (" + juce::String(periodDetail) + ")";
-        periodValueLabel->setText(tuning->getPeriodString(), juce::NotificationType::dontSendNotification);
+        if (tuning->getSizeString().isNotEmpty() && tuning->getPeriodString().isNotEmpty())
+            tokens.add(tuning->getSizeString() + " steps to " + tuning->getPeriodString());
+
+        else
+            tokens.add("Table size: " + juce::String(tuning->getTableSize()));
+
+        mappedTuningSummary = tokens.joinIntoString(" | ");
+
+        nameValueLabel->setText(mappedTuningSummary, juce::NotificationType::dontSendNotification);
+
+        //if (sizeString.isNotEmpty())
+        //    sizeString += " steps to ";
+        //sizeValueLabel->setText(sizeString, juce::NotificationType::dontSendNotification);
+
+        //periodValueLabel->setText(tuning->getPeriodString(), juce::NotificationType::dontSendNotification);
 
         resized();
     }
